35949c3c2cfc2d4da8204e4fbfa4c1d7060b5644
Spenzy <zieddr@gmail.com>
2020-11-17 13:36:16 +0100

    porting to master

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
+import {AssistanceComponent} from './assistance/assistance.component';
+import {ConsultingComponent} from './consulting/consulting.component';
+import {TrainingComponent} from './training/training.component';
+
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'training', component: TrainingComponent },
+  { path: 'consulting', component: ConsultingComponent },
+  { path: 'assistance', component: AssistanceComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ConsultingComponent } from './consulting/consulting.component';
 import { TrainingComponent } from './training/training.component';
 import { AssistanceComponent } from './assistance/assistance.component';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +20,8 @@ import { AssistanceComponent } from './assistance/assistance.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
