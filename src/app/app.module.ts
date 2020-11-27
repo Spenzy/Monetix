d43f225c5546b41219352f26688538b017a21d14
Spenzy <zieddr@gmail.com>
2020-11-28 00:14:14 +0100

    Created admin component and firebase service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormModalComponent } from './home/form-modal/form-modal.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
 
 
 
@@ -24,6 +25,7 @@ import { LoginComponent } from './login/login.component';
     HomeComponent,
     LoginComponent,
     FormModalComponent,
+    AdminComponent,
   ],
   imports: [
     BrowserModule,
